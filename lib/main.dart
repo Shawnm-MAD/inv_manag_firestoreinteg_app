380264fdf3e9d12e7df2ed720882ecbe08b09623
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-02 13:27:55 -0400

    Added firebase_core and firebase_option dependencies for flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // Import Flutter package
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 void main() {
 runApp(InventoryApp());
 }
