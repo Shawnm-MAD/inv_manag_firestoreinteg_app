304f5f02861ac15c010596ac608f940378fbae3a
Shawnm-cyber <shawnmathew14@gmail.com>
2025-04-02 13:32:58 -0400

    Initialize Firebase Setup for Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
-void main() {
-runApp(InventoryApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(InventoryApp());
 }
 class InventoryApp extends StatelessWidget {
 @override
